5a272a97d0ed484f97a6715db35967c791542427
DSM3 <fatec-dsm3@academico.intra>
2025-09-10 22:06:04 -0300

    10-09-2025

--- 05-autenticacao-com-jsonwebtoken/middleware/Auth.js
+++ 05-autenticacao-com-jsonwebtoken/middleware/Auth.js
@@ -5,11 +5,27 @@ import userController from '../controllers/userController.js'; //precisa do user
 //ele é válido
 
 const Authorization = (req, res, next) => {
-    const authToken = req.headers['authorization']//authorization é um campo padrão
+    const authToken = req.headers['authorization']//authorization é um campo padrão, aqui está gravado o token
     if (authToken != undefined){
-        next()
+        //O nosso tipo de token é Bearer (token portador), fica com o cliente
+        //Dividindo a string do token (para eliminar a palavra Bearer)
+        const bearer = authToken.split(" ")//cortar onde tiver um espaço, assim a palavra Bearer fica separada
+        const token = bearer[1]
+        //validando o token
+        jwt.verify(token, userController.JWTSecret, (error, data) => {
+            if (error){
+                res.status(401).json({error: "Token inválido"})//token não foi validado
+            } else {
+                req.token = token;
+                req.loggedUser = {
+                    id: data.id,
+                    email: data.email,
+                };
+                next() //next permite a passagem para o proximo passo da requisição
+            }
+        });        
     } else {
-        res.status(401).json({error: "Token invalido"});
+        res.status(401).json({error: "Acesso não-autorizado. Token invalido"});//caso token esteja vazio
     }
 };
 
